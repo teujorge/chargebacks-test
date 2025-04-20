5e104920607906e5b67b922410c840e6bd639832
Matheus Jorge <mrljorge@outlook.com>
2025-04-20 18:39:07 -0300

    style: update heading font to Roboto Condensed on HomePage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -12,7 +12,9 @@ export default function HomePage() {
     <div className="space-y-8">
       <Removable className="hidden md:block">
         <section className="flex flex-col gap-4 overflow-hidden rounded-xl bg-[rgba(196,228,255,0.08)]">
-          <h2 className="px-6 pt-6 text-2xl font-bold">In Case You Missed</h2>
+          <h2 className="font-roboto-condensed px-6 pt-6 text-2xl font-bold">
+            In Case You Missed
+          </h2>
           <ScrollFade className="flex flex-row gap-4 overflow-x-auto px-6 pb-4">
             <Suspense
               fallback={<VideoCardSkeletons className="h-fit min-w-80" />}
